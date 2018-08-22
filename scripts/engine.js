9b95a6744c2c54c3a5d1d46db105f3c1f4c1b866
Trzebu <trzebu@gmail.com>
2018-08-22 16:38:28 +0200

    Game Loop

--- scripts/engine.js
+++ scripts/engine.js
@@ -3,7 +3,63 @@
 var game_window = null;
 var ctx = null;
 
+var GameLoop = function () {
+    this.loopId = null;
+    this.lastFrameTimeMs = 0;
+    this.delta = 0;
+    this.fps = 60;
+    this.frameThisSecond = 0;
+    this.lastFpsUpdate = 0;
+
+    this.loop = function (timestamp) {
+        if (timestamp < this.lastFrameTimeMs + (1000 / 60)) {
+            window.requestAnimationFrame(this.loop.bind(this));
+            return;
+        }
+
+        this.delta += timestamp - this.lastFrameTimeMs;
+        this.lastFrameTimeMs = timestamp;
+
+        if (timestamp > (this.lastFpsUpdate + 1000)) {
+            this.fps = 0.25 * this.frameThisSecond + 0.75 * this.fps;
+            this.lastFpsUpdate = timestamp;
+            this.frameThisSecond = 0;
+        }
+
+        this.frameThisSecond++;
+        var numUpdateSteps = 0;
+
+        while(this.delta >= (1000 / 60)){
+            //logic update
+            this.delta -= 1000 / 60;
+            if(++numUpdateSteps >= 240){
+                this.panic();
+                break;
+            }
+        }
+
+        //animations
+
+        this.loopId = window.requestAnimationFrame(this.loop.bind(this));
+    }
+
+    this.panic = function () {
+        this.delta = 0;
+    }
+
+    this.stopLoop = function () {
+        window.cancelAnimationFrame(this.loopId);
+    }
+
+    this.startLoop = function () {
+        this.loopId = window.requestAnimationFrame(this.loop.bind(this));
+    }
+
+    this.startLoop();
+}
+
 var img = function () {
+    this.images = [];
     this.list = [
         "castle_wall",
         "kafelka",
@@ -17,7 +73,6 @@ var img = function () {
         "player",
         "st_floor"
     ];
-    this.images = [];
 
     this.get = function (id) {
         for (var i in this.images) {
@@ -42,7 +97,10 @@ var img = function () {
             object.onerror = function () {
                 console.error("Error while load queue. Can not load image " + this.list[i]);
             }.bind(this);
-            this.images.push({id: this.list[i], obj: object});
+            this.images.push({
+                id: this.list[i],
+                obj: object
+            });
         }
     }
 
