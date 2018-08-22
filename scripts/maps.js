56212273518c50cafef68f4f6a2a35509ed2980f
Trzebu <trzebu@gmail.com>
2018-08-22 17:38:40 +0200

    Loading map.

---
+++ scripts/maps.js
@@ -0,0 +1,3 @@
+var maps = [];
+
+maps.push("                                                                    cccc            cp.ccc          c.s..c         ccc.c.cc        cxc.c..c        cxs..c.c        cx...s.c        cccccccc                                                                     ");
